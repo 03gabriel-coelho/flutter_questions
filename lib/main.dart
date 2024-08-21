413ebd4ec589cbf684ed6cd83b3878db0cff12d4
Gabriel Freitas <gabriel.freitas@in8.com.br>
2024-08-21 10:41:57 -0300

    continuação do curso

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,46 @@
+import 'package:flutter/material.dart';
 
-// Iniciar aplicação
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var index = 0;
+
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
+    void responder() {
+      index++;
+      print(index);
+      print('Pergunta respondida');
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[index]),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
